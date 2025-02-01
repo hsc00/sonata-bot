e55403cb2613f4baf181b003a73eddefd7a096d4
Henrique <hscoutinho00@gmail.com>
2025-02-01 12:41:06 +0000

    refactor: sonatahelp command

--- commands/sonatahelp.py
+++ commands/sonatahelp.py
@@ -1,15 +1,18 @@
 import discord
 
 def setup(bot):
-    @bot.command(name='helpsonata')
-    async def helpsonata(ctx):
+    @bot.command(name='sonatahelp')
+    async def sonatahelp(ctx):
         # Read the content of help.txt
         with open('help.txt', 'r') as file:
             help_content = file.read()
 
+        # Replace the placeholder with the actual hyperlink
+        help_content = help_content.replace("[here]", "[here](https://google.com)")
+
         # Create an embed to display the help content
         embed = discord.Embed(
-            title="Help",
+            title="Welcome to Sonata Bot!",
             description=help_content,
             color=discord.Color.blue()
         )
