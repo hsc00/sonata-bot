f683f6b43e8ad86d79a6cba9f869833ced7dae89
Henrique <hscoutinho00@gmail.com>
2025-02-01 12:56:45 +0000

    Update sonatahelp.py

--- commands/sonatahelp.py
+++ commands/sonatahelp.py
@@ -8,7 +8,7 @@ def setup(bot):
             help_content = file.read()
 
         # Replace the placeholder with the actual hyperlink
-        help_content = help_content.replace("[here]", "[here](https://google.com)")
+        help_content = help_content.replace("[here]", "[here](https://discord.com/channels/1033860114026860645/1034393375291482112/1335231831284383825)")
 
         # Create an embed to display the help content
         embed = discord.Embed(
