69ce1a10a8f6324fa3b7805bec831aaf1eb8b71f
Henrique <hscoutinho00@gmail.com>
2025-02-03 22:26:05 +0000

    Create wholiked.py

---
+++ commands/wholiked.py
@@ -0,0 +1,19 @@
+import time
+from API.artist_cache import *
+
+def setup(bot):
+    @bot.command(name='wholikedartist')
+    async def wholiked(ctx):
+        async with ctx.message.channel.typing():
+            await who_liked_disliked_artist(ctx.message)
+            pass
+            time.sleep(5)
+
+    @bot.command(name='wla')
+    async def wl(ctx):
+        await who_liked_disliked_artist(ctx.message)
+        pass
+        time.sleep(5)
+
+async def who_liked_disliked_artist(message):
+    print("Who liked artist")
