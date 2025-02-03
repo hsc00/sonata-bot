39273e1c0f07396e54b00a346c093804817246fb
Henrique <hscoutinho00@gmail.com>
2025-02-03 18:18:10 +0000

    refactor: cooldowns fixed

--- commands/setlists.py
+++ commands/setlists.py
@@ -7,13 +7,13 @@ def setup(bot):
     async def setlists_command(ctx):
         async with ctx.message.channel.typing():
             await process_setlists(ctx.message)
-            time.sleep(5)
+        time.sleep(5)
 
     @bot.command(name='sts')
     async def sts_command(ctx):
         async with ctx.message.channel.typing():
             await process_setlists(ctx.message)
-            time.sleep(5)
+        time.sleep(5)
 
 async def process_setlists(message):
     content_parts = message.content.split(' ', 1)
