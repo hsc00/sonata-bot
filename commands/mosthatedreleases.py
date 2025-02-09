598adce23aa4de8ef95af352c27e3c42976836fb
Henrique <hscoutinho00@gmail.com>
2025-02-09 16:09:39 +0000

    feature: general updates to add most hated releases

--- commands/mosthatedreleases.py
+++ commands/mosthatedreleases.py
@@ -5,28 +5,28 @@ from API.album_cache import *
 
 
 def setup(bot):
-    @bot.command(name='mostlovedreleases')
-    async def mostlovedreleases(ctx):
+    @bot.command(name='mosthatedreleases')
+    async def mosthatedreleases(ctx):
         async with ctx.message.channel.typing():
-            await most_loved_releases(ctx)
+            await most_hated_releases(ctx)
             time.sleep(5)
 
-    @bot.command(name='mlr')
+    @bot.command(name='mhr')
     async def mlr(ctx):
         async with ctx.message.channel.typing():
-            await most_loved_releases(ctx)
+            await most_hated_releases(ctx)
             time.sleep(5)
 
-async def most_loved_releases(message):
-    most_loved = get_most_loved_hated_releases('liked')
+async def most_hated_releases(message):
+    most_loved = get_most_loved_hated_releases('disliked')
     leaderboard = []
 
     for idx, album in enumerate(most_loved, start=1):
         release_name = album.get('release_name')
         artist_name = album.get('artist_name')
         link = album.get('link')
-        liked_users_count = len(album.get('liked_users', []))
-        leaderboard.append(f"{idx}. [{artist_name} - {release_name}]({link}) ({liked_users_count} likes)")
+        liked_users_count = len(album.get('disliked_users', []))
+        leaderboard.append(f"{idx}. [{artist_name} - {release_name}]({link}) ({liked_users_count} dislikes)")
 
     # Split leaderboard into pages of 10 entries each
     pages = [leaderboard[i:i + 10] for i in range(0, len(leaderboard), 10)]
@@ -35,8 +35,8 @@ async def most_loved_releases(message):
 
     for i, page in enumerate(pages):
         embed_description = "\n".join(page)
-        embed_color = discord.Color.green()
-        embed = discord.Embed(title="Most Loved Releases", description=embed_description, color=embed_color)
+        embed_color = discord.Color.red()
+        embed = discord.Embed(title="Most Hated Releases", description=embed_description, color=embed_color)
         embed.set_footer(text=f"Requested by {message.author.display_name} • Page {i + 1}/{len(pages)}")
         if i == 0 and most_loved_image_url:
             embed.set_thumbnail(url=most_loved_image_url)
