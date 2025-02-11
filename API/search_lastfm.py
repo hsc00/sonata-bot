8f455bd09e4739927d4e6c9dd11d0a0d9bbd736b
Henrique <hscoutinho00@gmail.com>
2025-02-11 21:08:04 +0000

    fix: apostrophe fix

--- API/search_lastfm.py
+++ API/search_lastfm.py
@@ -124,6 +124,8 @@ def get_lastfm_track(user_id, data_type):
         return False 
 
 def clean_name(name):
+    # Replace apostrophes with a space
+    name = re.sub(r"'", " ", name)
     # Remove (remaster) and similar variants within parentheses
     name = re.sub(r'\s*\([^)]*remaster[^)]*\)', '', name, flags=re.IGNORECASE)
     # Remove remaster variants with hyphens or standalone
