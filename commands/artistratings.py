56f9a38e86d3f24d8abe1cce2bd8f20be4c5b6ec
Henrique <hscoutinho00@gmail.com>
2025-04-12 16:31:42 +0100

    feat: can search for the ratings of a user

--- commands/artistratings.py
+++ commands/artistratings.py
@@ -4,8 +4,8 @@ import re
 import discord
 import urllib
 from API.search_lastfm import get_lastfm_track
-from API.rym_search import search_rym_release, search_rym_artist
-from utils.text_formatters import add_newline_limit
+from API.rym_search import search_rym_artist
+from utils.text_formatters import *
 from views import Paginator
 from API.ratings_cache import ratings_cache
 
@@ -18,17 +18,20 @@ def setup(bot):
 async def get_artist_ratings(ctx):
     message = ctx.message
     release_query = get_release_query(message)
-    
-    if not release_query:
+
+    artist_name, user_id = get_user_id(release_query)
+    user_id = int(ctx.author.id) if user_id is None else int(user_id)
+
+    if not artist_name:
         await message.channel.send('Please provide an artist or use `!setfm` to set your last.fm account.')
         return
 
-    search_result = search_rym_artist(release_query)
+    search_result = search_rym_artist(artist_name)
     if not search_result:
-        await message.channel.send(f'**{release_query}** not found.')
+        await message.channel.send(f'**{artist_name.title()}** not found.')
         return
 
-    _, embeds = build_embed(ctx, search_result)
+    _, embeds = build_embed(ctx, search_result, user_id)
 
     if embeds:
         sent_message = await message.channel.send(embed=embeds[0])
@@ -41,10 +44,10 @@ def get_release_query(message):
     content_parts = message.content.split(' ', 1)
     return content_parts[1] if len(content_parts) > 1 else get_lastfm_track(message.author.id, 'artist')
 
-def build_embed(ctx, search_result):
+def build_embed(ctx, search_result, user_id):
     artist_name = search_result['artist_name']
     genres = search_result['genres']
-    user_id = int(ctx.author.id)
+
     if search_result.get('artist_img_url'):
         artist_img_url = search_result['artist_img_url']
     else:
@@ -63,7 +66,7 @@ def build_embed(ctx, search_result):
             sorted_ratings = sorted(ratings, key=lambda x: x.rating, reverse=True)
             
             for rating in sorted_ratings:
-                if (artist_name in rating.artist_name or artist_name in rating.artist_name_localized):
+                if (artist_name in rating.artist_name or artist_name in rating.artist_name_localized) and rating.rating != 0.0:
                     average, user_ratings, ratings_count = send_rating(rating, average, user_ratings, ratings_count)
 
             if ratings_count > 10:
@@ -71,12 +74,12 @@ def build_embed(ctx, search_result):
                 paginated_ratings = [user_ratings[i:i+10] for i in range(0, len(user_ratings), 10)]
                 for ratings_page in paginated_ratings:
                     user_ratings = "".join(ratings_page)
-                    embeds.append(create_embed(ctx, paginated_ratings, embed_title, embed_description, user_ratings, average, link, embed_color, artist_img_url, ctx.author.name))
+                    embeds.append(create_embed(ctx, user_id, paginated_ratings, embed_title, embed_description, user_ratings, average, link, embed_color, artist_img_url, ctx.author.name))
                 user_ratings, ratings_count, average = [], 0, (0, 0)  # Reset user_ratings as a list
 
     if ratings_count:
         user_ratings = "".join(user_ratings)
-        embeds.append(create_embed(ctx, None, embed_title, embed_description, user_ratings, average, link, embed_color, artist_img_url, ctx.author.name))
+        embeds.append(create_embed(ctx, user_id, None, embed_title, embed_description, user_ratings, average, link, embed_color, artist_img_url, ctx.author.name))
 
     return None, embeds
 
@@ -86,7 +89,7 @@ def send_rating(rating, average, user_ratings, ratings_count):
     average = (average[0] + rating.rating, average[1] + 1)
 
     star_rating = "📝" if rating.rating == "0" else "<:star:1338267791639445564>" * int(rating.rating) + "<:half:1338267704959828069> " * (1 if rating.rating != int(rating.rating) else 0)
-    rating_entry += f"**[{add_newline_limit(html.unescape(rating.title))}](https://rateyourmusic.com/search?searchtype=a&searchterm={urllib.parse.quote(rating.title)}&searchtype=)** • {'Unrated' if star_rating == 0.0 else star_rating}\n"
+    rating_entry += f"**[{add_newline_limit(html.unescape(rating.title))}](https://rateyourmusic.com/search?searchtype=a&searchterm={urllib.parse.quote(rating.title)}&searchtype=)** • {star_rating}\n"
 
     user_ratings.append(rating_entry)  # Append the rating_entry string to the list
     ratings_count += 1
@@ -94,15 +97,15 @@ def send_rating(rating, average, user_ratings, ratings_count):
     return average, user_ratings, ratings_count
 
 
-def create_embed(ctx, paginated_ratings, embed_title, embed_description, user_ratings, average, link, embed_color, artist_img_url, author_name):
+def create_embed(ctx, user_id, paginated_ratings, embed_title, embed_description, user_ratings, average, link, embed_color, artist_img_url, author_name):
     if average[1]:
         average = average[0] / average[1]
-        embed_description += f"\n\n<@{ctx.author.id}> Average Rating: **{round(average, 2)}** ⭐\n\n{user_ratings}"
+        embed_description += f"\n\n<@{user_id}>\n Average Rating: **{round(average, 2)}** ⭐\n\n{user_ratings}"
     embed = discord.Embed(title=embed_title, description=embed_description, url=link, color=embed_color)
     if artist_img_url:
         embed.set_thumbnail(url=artist_img_url)
     if paginated_ratings:
-        embed.set_footer(text=f"Requested by {author_name} • Page {len(paginated_ratings)}")
+        embed.set_footer(text=f"Requested by {author_name} • Page 1/{len(paginated_ratings)}")
     else:
         embed.set_footer(text=f"Requested by {author_name}")
     return embed
--- utils/text_formatters.py
+++ utils/text_formatters.py
@@ -1,3 +1,6 @@
+import re
+
+
 def add_newline_limit(text, limit=45):
     if len(text) <= limit:
         return text  # If text is within the limit, return as is
@@ -9,3 +12,14 @@ def add_newline_limit(text, limit=45):
 
     # Insert a newline after the last word within the limit
     return text[:cut_off] + "\n" + text[cut_off:].strip()
+
+def get_user_id(string):
+    # Use regex to find the user id
+    match = re.search(r'<@(\d+)>', string)
+    if match:
+        user_id = match.group(1)  # Extract only the numeric part
+        # Remove the user id from the original string
+        release_query = re.sub(r'<@\d+>', '', string).strip()
+        return release_query, user_id  # Return the modified string and user_id separately
+    
+    return string, None  # Return original string and None if no mention is found
